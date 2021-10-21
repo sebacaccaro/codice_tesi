fad35dd29df49c0ca187ab96e6c89110d98fced7
sebacaccaro <sebastianocaccaro@gmail.com>
2021-10-21 15:05:39 +0200

    integrato pipeline_def in perturbation_superpipeline

--- perturbazione/perturbation_superpipelines.py
+++ perturbazione/perturbation_superpipelines.py
@@ -1,6 +1,6 @@
 import json
 from pipeline import DetokenizerModule, SuperPipeline, TokenizerModule
-from pipelines_def import segmentation_pipeline, token_pipeline, token_pipeline_token_version
+from pipeline import Pipeline, SplitModuleGenerator, CharsSubModule, AddPunctuationModule, MergeWordHyphenModule, SplitWithCommaModule, TokenSubModule
 
 tokenSub_activated = True
 
@@ -8,6 +8,42 @@ with open("../Perturbazione/submatrix_extraction/error_matrix.json") as f:
     subData = json.load(f)
 
 altDict = {}
+
+
+def token_pipeline(p_charsub, sub_data):
+    pipeline = Pipeline()
+    pipeline.addModule(CharsSubModule(sub_data, p_charsub))
+    return pipeline
+
+
+def token_pipeline_token_version(p_tokensub, sub_data, altDict):
+    pipeline = Pipeline()
+    pipeline.addModule(TokenSubModule(sub_data, 5, altDict, p_tokensub))
+    return pipeline
+
+
+############# SEGMENTATION PIPELINE #######################
+
+
+def segmentation_pipeline(p_mergehyphen, p_splitcomma, p_split, p_punctadd):
+    pipeline = Pipeline()
+    pipeline.addModule(MergeWordHyphenModule(p_mergehyphen))
+    # Potrebbe essere possibile unire questi due moduli
+    pipeline.addModule(SplitWithCommaModule(p_splitcomma, ","))
+    pipeline.addModule(SplitModuleGenerator(p_split))
+    pipeline.addModule(AddPunctuationModule(p_punctadd, "."))
+    pipeline.addModule(AddPunctuationModule(p_punctadd / 2, ","))
+    pipeline.addModule(AddPunctuationModule(p_punctadd / 2, "'"))
+    return pipeline
+
+
+############# FULL Pipeline ######################################
+def fullPipeline(tkn_pipeline, sgm_pipeleine):
+    pipeline = Pipeline()
+    pipeline.concatPipeline(tkn_pipeline)
+    pipeline.concatPipeline(sgm_pipeleine)
+    return pipeline
+
 
 easySegPipeline = segmentation_pipeline(p_mergehyphen=0.001,
                                         p_splitcomma=0.001,
